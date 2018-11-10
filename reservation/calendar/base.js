e7602de3a1cd21697344a4298faa6538632d677c
wsubin <dnjstnqls@hanmail.net>
2018-11-11 01:45:35 +0900

    Add files via upload

---
+++ reservation/calendar/base.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+ $("#line").hide();
+ $("#line1").hide();
+ $("#line2").css('margin-top','-23px').css('position','fixed').css('margin-left','86%').css('margin-bottom','1px');
+ $("#line3").css('margin-top','-23px').css('position','fixed').css('margin-left','92%').css('text-decoration','none').css('font-weight','900');
+ $("#box1").css('height','20px').css('positon','fixed');
+});
